e95cdf652fa7dcba89f1024cfc01364f8589da2a
Nik Chizhov <chizhov.nik.s@gmail.com>
2024-06-29 23:00:22 +0900

    chore: add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="clang-tidy-nolint",
+    description="A pre-commit hook to lint clang-tidy NOLINT comments",
+    url="https://github.com/Kpada/pre-commit-hooks",
+    version="0.0.0",
+    packages=[
+        "hooks",
+    ],
+    scripts=[
+        "hooks/clang_tidy_nolint.py",
+    ],
+)
